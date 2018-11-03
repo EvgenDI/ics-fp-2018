309f2f81ed75e96757be97fefb1086ad38d84005
Alexander Veselov <Alexander_Veselov@yahoo.com>
2018-11-04 01:41:51 +0300

    Complete Prolog Task 3

--- exercises/prolog/3_quicksort.pl
+++ exercises/prolog/3_quicksort.pl
@@ -1,1 +1,25 @@
 % Определить предикат qsort(L, K) который для заданного списка целых чисел возвращает отсортированный 
+
+pivot(_, [], [], []).
+
+pivot(P, [H|T], [H|L], R) :-
+    P >= H,
+    pivot(P, T, L, R).
+
+pivot(P, [H|T], L, [H|R]) :-
+    pivot(P, T, L, R).
+
+qsort([], []).
+
+qsort([H|T], S) :-
+    pivot(H, T, L, R),
+    qsort(L, LS),
+    qsort(R, RS),
+    append(LS, [H|RS], S).
+
+% Test
+
+:- initialization
+    qsort([2, 7, 8, 5, 3, 1, 9, 6, 4, 0], S),
+    writeln(S),
+    halt.
